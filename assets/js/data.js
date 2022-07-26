5be7798c75f8f96ed7751eeff528eaa65a129bfd
SongHau <songhau2772003@gmail.com>
2022-07-26 20:44:56 +0700

    changes

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,8 +1,8 @@
 /* Heading Scroll */
-window.addEventListener("scroll", function () {
-  var header = this.document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 10);
-});
+window.addEventListener("scroll",function(){
+  var header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY>10);
+})
 /* Open Modal */
 const lgBtns = document.querySelectorAll(".js-login");
 const modal = document.querySelector(".modal");
@@ -41,3 +41,14 @@ signupLink.onclick = () => {
   signupBtn.click();
   return false;
 };
+
+/* Responsive slide bar menu */
+var menu = document.querySelector(".menu");
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector("header .menu .btn-mobile .close-btn");
+menuBtn.addEventListener("click",() => {
+  menu.classList.add('active');
+});
+closeBtn.addEventListener("click", () =>{
+  menu.classList.remove('active');
+});
