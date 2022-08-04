939ad33255c001048e2e3e6406ec85c94945ebe8
TTHeip <hieptt.2003@gmail.com>
2022-08-04 11:11:37 +0700

    commit

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,52 @@
+/* TURN OFF INSPECT */
+// window.onload = function () {
+//    document.addEventListener(
+//       "contextmenu",
+//       function (e) {
+//          e.preventDefault();
+//       },
+//       false
+//    );
+//    document.addEventListener(
+//       "keydown",
+//       function (e) {
+//          // "I" key
+//          if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+//             disabledEvent(e);
+//          }
+//          // "J" key
+//          if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+//             disabledEvent(e);
+//          }
+//          // "S" key + macOS
+//          if (
+//             e.keyCode == 83 &&
+//             (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+//          ) {
+//             disabledEvent(e);
+//          }
+//          // "U" key
+//          if (e.ctrlKey && e.keyCode == 85) {
+//             disabledEvent(e);
+//          }
+//          // "F12" key
+//          if (event.keyCode == 123) {
+//             disabledEvent(e);
+//          }
+//       },
+//       false
+//    );
+//    function disabledEvent(e) {
+//       if (e.stopPropagation) {
+//          e.stopPropagation();
+//       } else if (window.event) {
+//          window.event.cancelBubble = true;
+//       }
+//       e.preventDefault();
+//       return false;
+//    }
+// };
+
 /* HEADING SCROLL */
 window.addEventListener("scroll", function () {
    var header = document.querySelector("header");
@@ -236,7 +285,7 @@ function changeCarousel(
    }
    carousel.style = `transform: translateX(${
       -carouselWidth * carouselNumber.number
-   }px)`;
+      }px)`;
 }
 for (let i = 0; i < nxtBtn.length; i++) {
    nxtBtn[i].addEventListener("click", function () {
