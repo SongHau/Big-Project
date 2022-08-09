3bffd91cb3ac6ef6f87b4a1d7898c691ee008dad
SongHau <songhau2772003@gmail.com>
2022-08-10 01:44:08 +0700

    changes

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -1,3 +1,22 @@
+window.addEventListener("scroll",reveal);
+function reveal(){
+   var reveals = document.querySelectorAll(".reveal");
+   for(var i = 0; i<reveals.length;i++)
+   {
+      var windowHeight = window.innerHeight;
+      var revealTop = reveals[i].getBoundingClientRect().top;
+      var revealPoint = 150;
+
+      if(revealTop < windowHeight - revealPoint)
+      {
+         reveals[i].classList.add("active");
+      }
+      else
+      {
+         reveals[i].classList.remove("active");
+      }
+   }
+}
 /* Method */
 const method = document.getElementById("pay-online");
 const choose = document.getElementById("js-choose");
@@ -8,3 +27,5 @@ method.addEventListener("click",function(){
 payMethod.addEventListener("click",function(){
    choose.style.opacity="0";
 })
+
+/* Scroll Element */
